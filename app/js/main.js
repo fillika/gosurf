0a956e09a6e431131739416e7fc8cde7a858672b
fillika <49833052+fillika@users.noreply.github.com>
2019-10-16 21:30:26 +0300

    Связал 2 слайдера, добавил стили и окрасил линию у текущего слайда

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,13 @@ $(function() {
     infinite: true,
     fade: true,
     nextArrow: '<img class="slick-arrow slick-arrow__right" src="img/arrow-right.svg" alt="" />',
-    prevArrow: '<img class="slick-arrow slick-arrow__left" src="img/arrow-left.svg" alt="" />'
+    prevArrow: '<img class="slick-arrow slick-arrow__left" src="img/arrow-left.svg" alt="" />',
+    asNavFor: ".slider-dots"
   });
+
+  $(".slider-dots").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: ".header__slider"
+  })
 });
