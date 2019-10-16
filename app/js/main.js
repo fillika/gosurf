164402f51695e17c98c2ec1f7c2308e817ced73d
fillika <49833052+fillika@users.noreply.github.com>
2019-10-16 09:07:02 +0300

    Настроил стрелку для переключений слайдера

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,8 @@
 $(function() {
   $(".header__slider").slick({
     infinite: true,
-    fade: true
+    fade: true,
+    nextArrow: '<img class="slick-arrow slick-arrow__right" src="img/arrow-right.svg" alt="" />',
+    prevArrow: '<img class="slick-arrow slick-arrow__left" src="img/arrow-left.svg" alt="" />'
   });
 });
