4f2e54ef3d4d85df04cbbb033ac1c9befaa45987
fillika <49833052+fillika@users.noreply.github.com>
2019-10-18 21:34:05 +0300

    Развернул строку, приступаю к стилизации кнопки на каждом слайде

--- app/js/main.js
+++ app/js/main.js
@@ -2,8 +2,10 @@ $(function() {
   $(".header__slider").slick({
     infinite: true,
     fade: true,
-    nextArrow: '<img class="slick-arrow slick-arrow__right" src="img/arrow-right.svg" alt="" />',
-    prevArrow: '<img class="slick-arrow slick-arrow__left" src="img/arrow-left.svg" alt="" />',
+    nextArrow:
+      '<img class="slick-arrow slick-arrow__right" src="img/arrow-right.svg" alt="" />',
+    prevArrow:
+      '<img class="slick-arrow slick-arrow__left" src="img/arrow-left.svg" alt="" />',
     asNavFor: ".slider-dots"
   });
 
@@ -11,5 +13,14 @@ $(function() {
     slidesToShow: 4,
     slidesToScroll: 1,
     asNavFor: ".header__slider"
-  })
+  });
+
+  $(".surf-slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow:
+      '<img class="slick-arrow slick-arrow__right" src="img/arrow-right.svg" alt="" />',
+    prevArrow:
+      '<img class="slick-arrow slick-arrow__left" src="img/arrow-left.svg" alt="" />'
+  });
 });
